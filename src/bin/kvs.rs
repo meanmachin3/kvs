03ab41ad28d2333abfda25891ecb9073f4da4fc4
Manish Yadav <yadavmanish1607@gmail.com>
2022-10-29 12:36:45 -0700

    ✨ Feat: added get, set and rm lib implementation

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,41 @@
+use clap::{Parser, Subcommand};
+use std::process::exit;
+
+#[derive(Parser)]
+#[command(author, version, about)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Get { key: String },
+    Set { key: String, value: String },
+    Rm { key: String },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    match &cli.command {
+        Commands::Get { key } => {
+            eprintln!("unimplemented");
+            exit(1);
+            // println!("'kvs get' was used, key is: {:?}", key)
+        }
+        Commands::Set { key, value } => {
+            eprintln!("unimplemented");
+            exit(1);
+            // println!("'kvs set' was used, key is: {:?} and value is: {:?}", key, value)
+        }
+        Commands::Rm { key } => {
+            eprintln!("unimplemented");
+            exit(1);
+            // println!("'kvs rm' was used, key is: {:?}", key)
+        }
+        _ => unreachable!(),
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,42 @@
+#![deny(missing_docs)]
+//! The KvStore stores key/value pairs.
+use std::collections::HashMap;
+
+/// Key/value pairs are stored in `HashMap` in memory and not persisted to disk.
+///
+/// Usage:
+/// ```
+/// use kvs::KvStore;
+///
+/// ```
 #[derive(Default)]
 pub struct KvStore {
-    
+    map: HashMap<String, String>,
 }
 
-
 impl KvStore {
+    /// Constructor for `KvStore`
     pub fn new() -> KvStore {
         KvStore {
-
+            map: HashMap::new(),
         }
     }
 
-    pub fn set(&self, _key: String, _value: String) {
-
+    /// Stores the value of a string key to a string
+    /// In case of collision, previous value would be overwritten.
+    pub fn set(&mut self, key: String, value: String) {
+        self.map.insert(key, value);
     }
 
-    pub fn get(&self, _key: String) -> Option<String> {
-        None
+    /// Stores the value of a string key to a string
+    ///
+    /// Returns `None` in case the value does not exist in hashmap
+    pub fn get(&self, key: String) -> Option<String> {
+        self.map.get(&key).cloned()
     }
 
-    pub fn remove(&self, _key: String) {
-
+    /// Removes the key from the in-memory store.
+    pub fn remove(&mut self, key: String) {
+        self.map.remove(&key);
     }
 }
